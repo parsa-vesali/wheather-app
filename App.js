1153f344ba09778a62c64bae42f591ddd91fd21c
Parsa Vesali <parsavesali84@gmail.com>
2024-03-12 10:32:35 +0330

    first commit

--- App.js
+++ App.js
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-teal-700">
+      <Text className>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreens.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeScreens() {
+  return <div>HomeScreens</div>;
+}
+
+export default HomeScreens;
